ce6d5438c6b2f64fcb442b49da69e976543f5850
VarunChowdary0 <saivarunchowdarypoludasu4248@gmail.com>
2023-08-10 19:30:15 +0530

    m 58

--- Module - 5/M_5_p_B-3.c
+++ Module - 5/M_5_p_B-3.c
@@ -3,8 +3,38 @@ WWrite a C program to
 read a text file “sample.txt”
 and print the following.
 a) Substring of N characters
-from theposition I.
+from the position I.
 b) Reverse order of
 substring of N characters
-producedin a
+produced in a
 */
+
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+    FILE *fp;
+    char ch,str[300];
+    int I,N,i;
+    fp=fopen("sample.txt","r");
+    if(fp == NULL )
+    {
+        printf("File not Found");
+    }
+    else
+    {
+        printf("Enter I and N: ");
+        scanf("%d%d",&I,&N );
+        fseek( fp , I , SEEK_SET );
+        for(i=0;i<N-I;i++)
+        {
+            ch = fgetc(fp);
+            printf("%c",ch);
+            str[i]=ch; 
+        }
+        printf("\n");
+        puts(strrev(str));
+        fclose(fp);
+    }
+}
